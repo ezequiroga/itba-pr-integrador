f0b2e724e8f3fee4b65395b9de3ee8705c87c6d5
Eze Q <ae.quiroga.88@gmail.com>
2020-11-19 21:58:19 -0300

    Se agrega funciones de ingreso de acciones y refactor de funciones

--- prfinal.py
+++ prfinal.py
@@ -1,12 +1,75 @@
 import os
 
-def getNomAcciones():
-    arcAcciones = os.listdir('stocks/')
-    nomAcciones = []
+nomAcciones = {}
+accionesParaAnalizar = {}
 
-    for unaAcc in arcAcciones:
-        nomAcciones.append(unaAcc.split('.')[0])
+def getNomAcciones():
+    """
+        Recorre el directorio de acciones y obtiene los nombres a partir de los nombres de los archivos.
+    """
+    if (len(nomAcciones) == 0):
+        arcAcciones = os.listdir('stocks/')
+
+        for i, unaAcc in enumerate(arcAcciones):
+            nomAcciones[i+1] = unaAcc.split('.')[0]
 
     return nomAcciones
 
-print(getNomAcciones())
+def isIdAccionIngresadoValido(id):
+    """
+        Verifica que id ingresado se corresponda con una accion elegible.
+    """
+    max = len(nomAcciones)
+    try:
+        id = int(id)
+    except:
+        return False
+    return (1 <= id <= max)
+
+def isTrueOrFalse(txt):
+    """
+        Verifica que los valores associados a 'si' y 'no' sean correctos.
+    """
+    return (txt == 'S' or txt == 's' or txt == 'N' or txt == 'n')
+
+def ingresoUnIdAccion():
+    """
+        Solicita al usuario el ingreso del ID de una acciones.
+    """
+    print("\nIngrese el ID de una accion:", end=' ')
+    idAccion = input()
+    while (not isIdAccionIngresadoValido(idAccion)):
+        print('{:-^30}'.format("Id inválido. Intente nuevamente:"), end =' ')
+        idAccion = input()    
+    accionesParaAnalizar[idAccion] = 1
+
+def preguntarSeguirIngresando():
+    """
+        Pregunta al usuario si desea seguir ingresando acciones para analizar.
+    """
+    print("Desea analizar otra acción s/n:", end = ' ')
+    tof = input()
+    while(not isTrueOrFalse(tof)):
+        print('{:-^30}'.format("Respuesta incorrecta. Intente nuevamente:"), end =' ')
+        tof = input()
+    return tof
+
+def ingresoDeAccionesAAnalizar():
+    """
+        Inicia el proceso de ingreso de acciones por analizar
+    """
+    print("{:*^50}".format("Acciones dispobles de ser analizadas"))
+    acciones = getNomAcciones()
+    
+    for unaAccion in acciones:
+        print(unaAccion, ':', nomAcciones[unaAccion])
+
+    seguirIngresando = True
+    while (seguirIngresando):
+        ingresoUnIdAccion()
+        tof = preguntarSeguirIngresando()
+        seguirIngresando = (tof == 's' or tof == 'S')
+
+
+
+ingresoDeAccionesAAnalizar()
